ea238b2e0527730971cfda91439524a38ff519f9
Wells Lucas Santo <theonlydevking@gmail.com>
2021-01-12 01:46:38 -0800

    Basic GET and POST routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+let books = [];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {books: books});
+});
+
+router.get('/books', function(req, res) {
+  res.json({ books: books});
+});
+
+router.post('/books', function(req, res) {
+  books.push(req.body.title);
+  res.redirect('/');
 });
 
 module.exports = router;
